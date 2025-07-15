1e7faa2881fe4301ed54f5bc74c4293e51404f17
NStrowbridge <2694872s@student.gla.ac.uk>
2025-07-15 18:01:16 +0100

    Updated script

--- Differential_expression/05.4_UpSet_plot_graph.R
+++ Differential_expression/05.4_UpSet_plot_graph.R
@@ -2,43 +2,45 @@
 library(dplyr)
 library(ComplexUpset)
 library(ggplot2)
-library(dplyr)
 library(ggtext)
 library(stringr)
 
 ####~housekeeping~~~~~~~~~~~~####
-
-rm(list=ls()) #clear the environment
-setwd("/Users/nicstrowbridge/Desktop/Nic_PhD_files_2/DirectRNA_Colour_bernardezi/Differential_expression/01_scripts") #set wd
+rm(list = ls()) # clear the environment
+setwd("/Users/nicstrowbridge/Desktop/Nic_PhD_files_2/DirectRNA_Colour_bernardezi/Differential_expression/01_scripts") # set wd
 
 ###~~output dir~~~~~~~~~~~~~~####
 output = "../05.2_dge_graphs_analysis_colour_loci_gene_level/"
-dir.create(output) #make folder for output
+dir.create(output, showWarnings = FALSE) # make folder for output
 setwd(output)
 
 ####~~~load data~~~####
 
 # Load methylation data
 meth_yelvbla <- read.csv("/Users/nicstrowbridge/Desktop/Nic_PhD_files_2/DirectRNA_Colour_bernardezi/Differential_methylation/02_reference_data/YelvBla_DM_anno.csv")
-colnames(meth_yelvbla) = meth_yelvbla[1, ]
-meth_yelvbla <- meth_yelvbla[-c(1),]
+colnames(meth_yelvbla) <- meth_yelvbla[1, ]
+meth_yelvbla <- meth_yelvbla[-1, ]
+
 meth_brovbla <- read.csv("/Users/nicstrowbridge/Desktop/Nic_PhD_files_2/DirectRNA_Colour_bernardezi/Differential_methylation/02_reference_data/BrovBla_DM_anno.csv")
-colnames(meth_brovbla) = meth_brovbla[1, ]
-meth_brovbla <- meth_brovbla[-c(1),]
+colnames(meth_brovbla) <- meth_brovbla[1, ]
+meth_brovbla <- meth_brovbla[-1, ]
+
 meth_yelvbro <- read.csv("/Users/nicstrowbridge/Desktop/Nic_PhD_files_2/DirectRNA_Colour_bernardezi/Differential_methylation/02_reference_data/YelvBroM_DM_anno.csv")
-colnames(meth_yelvbro) = meth_yelvbro[1, ]
-meth_yelvbro <- meth_yelvbro[-c(1),]
+colnames(meth_yelvbro) <- meth_yelvbro[1, ]
+meth_yelvbro <- meth_yelvbro[-1, ]
 
 # Load expression data
 expr_yelvbla <- read.csv("/Users/nicstrowbridge/Desktop/Nic_PhD_files_2/DirectRNA_Colour_bernardezi/Differential_expression/02_reference_data/YelvBla_DE_anno.csv")
-colnames(expr_yelvbla) = expr_yelvbla[1, ]
-expr_yelvbla <- expr_yelvbla[-c(1),]
+colnames(expr_yelvbla) <- expr_yelvbla[1, ]
+expr_yelvbla <- expr_yelvbla[-1, ]
+
 expr_brovbla <- read.csv("/Users/nicstrowbridge/Desktop/Nic_PhD_files_2/DirectRNA_Colour_bernardezi/Differential_expression/02_reference_data/BrovBla_DE_anno.csv")
-colnames(expr_brovbla) = expr_brovbla[1, ]
-expr_brovbla <- expr_brovbla[-c(1),]
+colnames(expr_brovbla) <- expr_brovbla[1, ]
+expr_brovbla <- expr_brovbla[-1, ]
+
 expr_yelvbro <- read.csv("/Users/nicstrowbridge/Desktop/Nic_PhD_files_2/DirectRNA_Colour_bernardezi/Differential_expression/02_reference_data/YelvBroM_DE_anno.csv")
-colnames(expr_yelvbro) = expr_yelvbro[1, ]
-expr_yelvbro <- expr_yelvbro[-c(1),]
+colnames(expr_yelvbro) <- expr_yelvbro[1, ]
+expr_yelvbro <- expr_yelvbro[-1, ]
 
 # Extract Putative Gene columns
 genes_yelvbla_meth <- meth_yelvbla$`Putative Gene`
@@ -66,19 +68,30 @@ gene_matrix <- data.frame(Gene = all_genes) %>%
     "Yellow vs Brown Expression" = Gene %in% genes_yelvbro_expr
   )
 
-# Create the plot
+# Rename columns with colored HTML labels
+colnames(gene_matrix)[2:7] <- c(
+  "<span style='color:gold;'>Yellow</span> vs <span style='color:black;'>Black</span> Methylation",
+  "<span style='color:gold;'>Yellow</span> vs <span style='color:black;'>Black</span> Expression",
+  "<span style='color:saddlebrown;'>Brown</span> vs <span style='color:black;'>Black</span> Methylation",
+  "<span style='color:saddlebrown;'>Brown</span> vs <span style='color:black;'>Black</span> Expression",
+  "<span style='color:gold;'>Yellow</span> vs <span style='color:saddlebrown;'>Brown</span> Methylation",
+  "<span style='color:gold;'>Yellow</span> vs <span style='color:saddlebrown;'>Brown</span> Expression"
+)
+
+
+# Create the upset plot
 upset_plot_meth_expr <- upset(
   gene_matrix,
-  intersect = c(
-    "Yellow vs Black Methylation", "Yellow vs Black Expression",
-    "Brown vs Black Methylation", "Brown vs Black Expression",
-    "Yellow vs Brown Methylation", "Yellow vs Brown Expression"
-  ),
+  intersect = colnames(gene_matrix)[2:7],
   name = "Putative Gene Overlap",
   width_ratio = 0.25,
-  base_annotations=list(
-    'Intersection size'=intersection_size(counts=FALSE)
+  base_annotations = list(
+    'Intersection size' = intersection_size(counts = FALSE)
   )
-    )
+) +
+  theme(
+    axis.text.y = element_markdown(size = 10)
+  )
 
+# Save the plot
 ggsave("upset_plot_meth_expr.svg", upset_plot_meth_expr, width = 14, height = 6)
