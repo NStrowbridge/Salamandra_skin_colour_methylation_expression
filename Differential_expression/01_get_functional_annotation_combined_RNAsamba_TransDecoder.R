f655c2c80a4f61934fe51e7bd78b66e4e30426e7
NStrowbridge <2694872s@student.gla.ac.uk>
2026-02-23 13:12:22 +0000

    Full annotation

--- Differential_expression/01_get_functional_annotation_combined_RNAsamba_TransDecoder.R
+++ Differential_expression/01_get_functional_annotation_combined_RNAsamba_TransDecoder.R
@@ -61,26 +61,105 @@ result <- overlap_dataframes(eggnog_transdecoder, eggnog_rnasamba)
 
 final_annotated_transcripts <- function(merged_df) {
   # Define the columns to retain
-  columns_to_retain <- c("Transcript_ID", "seed_ortholog", "Preferred_name", "GOs", "score_transdecoder", "score_rnasamba")
+  columns_to_retain <- c("Transcript_ID", "seed_ortholog", "Preferred_name", "GOs", "Description", "KEGG_ko", "KEGG_Pathway", "KEGG_Module", "KEGG_Reaction", "KEGG_rclass", "PFAMs",  "score_transdecoder", "score_rnasamba")
   
   # Create a new dataframe to store the final list of annotated transcripts
   final_df <- merged_df %>%
     mutate(
-      seed_ortholog = ifelse(!is.na(seed_ortholog_transdecoder) & !is.na(seed_ortholog_rnasamba), 
-                             ifelse(score_transdecoder >= score_rnasamba, seed_ortholog_transdecoder, seed_ortholog_rnasamba),
-                             coalesce(seed_ortholog_transdecoder, seed_ortholog_rnasamba)),
-      Preferred_name = ifelse(!is.na(Preferred_name_transdecoder) & !is.na(Preferred_name_rnasamba), 
-                              ifelse(score_transdecoder >= score_rnasamba, Preferred_name_transdecoder, Preferred_name_rnasamba),
-                              coalesce(Preferred_name_transdecoder, Preferred_name_rnasamba)),
-      GOs = ifelse(!is.na(GOs_transdecoder) & !is.na(GOs_rnasamba), 
-                   ifelse(score_transdecoder >= score_rnasamba, GOs_transdecoder, GOs_rnasamba),
-                   coalesce(GOs_transdecoder, GOs_rnasamba))
+      seed_ortholog = ifelse(
+        !is.na(seed_ortholog_transdecoder) & !is.na(seed_ortholog_rnasamba),
+        ifelse(score_transdecoder >= score_rnasamba,
+               seed_ortholog_transdecoder, seed_ortholog_rnasamba),
+        ifelse(!is.na(seed_ortholog_transdecoder),
+               seed_ortholog_transdecoder, seed_ortholog_rnasamba)
+      ),
+      
+      Preferred_name = ifelse(
+        !is.na(Preferred_name_transdecoder) & !is.na(Preferred_name_rnasamba),
+        ifelse(score_transdecoder >= score_rnasamba,
+               Preferred_name_transdecoder, Preferred_name_rnasamba),
+        ifelse(!is.na(Preferred_name_transdecoder),
+               Preferred_name_transdecoder, Preferred_name_rnasamba)
+      ),
+      
+      GOs = ifelse(
+        !is.na(GOs_transdecoder) & !is.na(GOs_rnasamba),
+        ifelse(score_transdecoder >= score_rnasamba,
+               GOs_transdecoder, GOs_rnasamba),
+        ifelse(!is.na(GOs_transdecoder),
+               GOs_transdecoder, GOs_rnasamba)
+      ),
+      
+      Description = ifelse(
+        !is.na(Description_transdecoder) & !is.na(Description_rnasamba),
+        ifelse(score_transdecoder >= score_rnasamba,
+               Description_transdecoder, Description_rnasamba),
+        ifelse(!is.na(Description_transdecoder),
+               Description_transdecoder, Description_rnasamba)
+      ),
+      
+      KEGG_ko = ifelse(
+        !is.na(KEGG_ko_transdecoder) & !is.na(KEGG_ko_rnasamba),
+        ifelse(score_transdecoder >= score_rnasamba,
+               KEGG_ko_transdecoder, KEGG_ko_rnasamba),
+        ifelse(!is.na(KEGG_ko_transdecoder),
+               KEGG_ko_transdecoder, KEGG_ko_rnasamba)
+      ),
+      
+      KEGG_Pathway = ifelse(
+        !is.na(KEGG_Pathway_transdecoder) & !is.na(KEGG_Pathway_rnasamba),
+        ifelse(score_transdecoder >= score_rnasamba,
+               KEGG_Pathway_transdecoder, KEGG_Pathway_rnasamba),
+        ifelse(!is.na(KEGG_Pathway_transdecoder),
+               KEGG_Pathway_transdecoder, KEGG_Pathway_rnasamba)
+      ),
+      
+      KEGG_Module = ifelse(
+        !is.na(KEGG_Module_transdecoder) & !is.na(KEGG_Module_rnasamba),
+        ifelse(score_transdecoder >= score_rnasamba,
+               KEGG_Module_transdecoder, KEGG_Module_rnasamba),
+        ifelse(!is.na(KEGG_Module_transdecoder),
+               KEGG_Module_transdecoder, KEGG_Module_rnasamba)
+      ),
+      
+      KEGG_Reaction = ifelse(
+        !is.na(KEGG_Reaction_transdecoder) & !is.na(KEGG_Reaction_rnasamba),
+        ifelse(score_transdecoder >= score_rnasamba,
+               KEGG_Reaction_transdecoder, KEGG_Reaction_rnasamba),
+        ifelse(!is.na(KEGG_Reaction_transdecoder),
+               KEGG_Reaction_transdecoder, KEGG_Reaction_rnasamba)
+      ),
+      
+      KEGG_rclass = ifelse(
+        !is.na(KEGG_rclass_transdecoder) & !is.na(KEGG_rclass_rnasamba),
+        ifelse(score_transdecoder >= score_rnasamba,
+               KEGG_rclass_transdecoder, KEGG_rclass_rnasamba),
+        ifelse(!is.na(KEGG_rclass_transdecoder),
+               KEGG_rclass_transdecoder, KEGG_rclass_rnasamba)
+      ),
+      
+      PFAMs = ifelse(
+        !is.na(PFAMs_transdecoder) & !is.na(PFAMs_rnasamba),
+        ifelse(score_transdecoder >= score_rnasamba,
+               PFAMs_transdecoder, PFAMs_rnasamba),
+        ifelse(!is.na(PFAMs_transdecoder),
+               PFAMs_transdecoder, PFAMs_rnasamba)
+      )
     ) %>%
-    dplyr::select(Transcript_ID, seed_ortholog, Preferred_name, GOs) %>%
-    filter(!is.na(seed_ortholog) & !is.na(Preferred_name) & !is.na(GOs))
+    dplyr::select(
+      Transcript_ID, seed_ortholog, Preferred_name, GOs,
+      Description, KEGG_ko, KEGG_Pathway, KEGG_Module,
+      KEGG_Reaction, KEGG_rclass, PFAMs
+    ) %>%
+    filter(
+      !is.na(seed_ortholog),
+      !is.na(Preferred_name),
+      !is.na(GOs)
+    )
   
   return(final_df)
 }
+
 
 # Example usage with the merged dataframe from the previous function
 result <- overlap_dataframes(eggnog_transdecoder, eggnog_rnasamba)
@@ -92,6 +171,7 @@ print(duplicates)
 
 transcriptome_annotaton_combined_RNAsamba_Transdecoder <- final_result %>% distinct(Transcript_ID, .keep_all = TRUE)
 
+
 transcriptome_annotaton_combined_RNAsamba_Transdecoder_dups <- transcriptome_annotaton_combined_RNAsamba_Transdecoder %>% filter(duplicated(Transcript_ID))
 print("\nDuplicates in Transcript_ID column:")
 print(transcriptome_annotaton_combined_RNAsamba_Transdecoder)
@@ -102,9 +182,10 @@ transcriptome_annotaton_combined_RNAsamba_Transdecoder <- transcriptome_annotato
 print(head(transcriptome_annotaton_combined_RNAsamba_Transdecoder))
 txname_geneid = transcriptome_annotaton_combined_RNAsamba_Transdecoder[,c("Transcript_ID","GENEID")]
 
-###~~make txname*Preferred_name+GO~~~~~~~~~~####
+###~~make annotation files~~~~~~~~~~####
 txname_Preferredname_GOcat = transcriptome_annotaton_combined_RNAsamba_Transdecoder[,c("Transcript_ID","Preferred_name", "GOs")]
 txname_Preferredname = transcriptome_annotaton_combined_RNAsamba_Transdecoder[,c("Transcript_ID","Preferred_name")]
+txname_all_annotations = transcriptome_annotaton_combined_RNAsamba_Transdecoder[,c("Transcript_ID", "GENEID", "Preferred_name", "GOs", "Description", "KEGG_ko", "KEGG_Pathway", "KEGG_Module", "KEGG_Reaction", "KEGG_rclass", "PFAMs")]
 
 ####~map file for topGO~~~~~~####
 gene2GO = transcriptome_annotaton_combined_RNAsamba_Transdecoder[,c("GENEID","GOs")] #make gene2GO annotation list
@@ -114,9 +195,8 @@ write.csv(transcriptome_annotaton_combined_RNAsamba_Transdecoder, file = "../02_
 write.csv(txname_geneid, file = "../02_reference_data/txname_geneid.csv")
 write.csv(txname_Preferredname_GOcat, file = "../02_reference_data/txname_Preferredname_GOcat.csv")
 write.csv(txname_Preferredname, file = "../02_reference_data/txname_Preferredname.csv")
+write.csv(txname_all_annotations, file = "../02_reference_data/txname_all_annotations.csv")
 write_tsv(gene2GO, file = "../02_reference_data/gene2GO.map", col_names = FALSE)
-
+write.csv(result$merged_df, file = "../02_reference_data/merged_df.csv")
 ####~fin~~~~~~~~~~~~~~~~~~~~~####
-
 closeAllConnections()
-
